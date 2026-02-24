e31d860382a8afeb61db63ed94f0f293ba6d7e34
Gabriel100-Pro <cortesempreender2.0@gmail.com>
2026-02-24 03:34:51 -0300

    Resolvendo problema do icon de hamburger

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
+ const menuToggle = document.getElementById("menuToggle");
+const sidebar = document.getElementById("sidebar");
+const overlay = document.getElementById("overlay");
+
+menuToggle.addEventListener("click", () => {
+    sidebar.classList.toggle("active");
+    overlay.classList.toggle("active");
+});
+
+overlay.addEventListener("click", () => {
+    sidebar.classList.remove("active");
+    overlay.classList.remove("active");
+});
+
+
 function loadPage(page) {
     const content = document.getElementById("content");
 
@@ -178,18 +193,6 @@ function initCharts() {
         }
     });
 
-    const menuToggle = document.getElementById("menuToggle");
-const sidebar = document.getElementById("sidebar");
-const overlay = document.getElementById("overlay");
-
-menuToggle.addEventListener("click", () => {
-    sidebar.classList.toggle("active");
-    overlay.classList.toggle("active");
-});
-
-overlay.addEventListener("click", () => {
-    sidebar.classList.remove("active");
-    overlay.classList.remove("active");
-});
+   
 }
 loadPage("dashboard");
