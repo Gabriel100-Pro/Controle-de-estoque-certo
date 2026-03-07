0e1bf08bfb6674656f34f5eb086c65e04896e78b
Gabriel100-Pro <cortesempreender2.0@gmail.com>
2026-03-07 02:35:14 -0300

    Exclusão de códigos desnecessários.

---
+++ aula.py
@@ -0,0 +1,21 @@
+import turtle
+t = turtle.Screen()
+t.bgcolor("white")
+t.title("AMinha Primeira Tela")
+
+t = turtle.Turtle("turtle")
+t.speed(1)
+t.color("blue")
+
+
+t.forward(200)
+t.left(120)
+t.forward(200)
+t.left(120)
+t.forward(200)
+
+turtle.done()
+
+# 2410367
+# 2403736
+# 2401942
